42a1c4d80f3fc998d50bd73757e4bbda2c7d270f
Simon Claret <sclaret@gmail.com>
2023-10-24 22:51:42 -0400

    Import bootstrap scss
    From https://github.com/twbs/examples/tree/main/vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-export default function App() {
+import { TestIconList } from "./TestIconList.jsx";
+
+export function App() {
   return (
-    <div className="card bg-warning">
-      <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+    <>
+      <div className="card bg-warning">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
       </div>
-    </div>
+      <TestIconList />
+    </>
   );
 }
---
+++ src/TestIconList.jsx
@@ -0,0 +1,31 @@
+export function TestIconList() {
+  return (
+    <ul className="icon-list">
+      <li>
+        <a href="https://getbootstrap.com/docs/5.3/getting-started/introduction/">
+          Bootstrap quick start guide
+        </a>
+      </li>
+      <li>
+        <a href="https://getbootstrap.com/docs/5.3/getting-started/webpack/">
+          Bootstrap Webpack guide
+        </a>
+      </li>
+      <li>
+        <a href="https://getbootstrap.com/docs/5.3/getting-started/parcel/">
+          Bootstrap Parcel guide
+        </a>
+      </li>
+      <li>
+        <a href="https://getbootstrap.com/docs/5.3/getting-started/vite/">
+          Bootstrap Vite guide
+        </a>
+      </li>
+      <li>
+        <a href="https://getbootstrap.com/docs/5.3/getting-started/contribute/">
+          Contributing to Bootstrap
+        </a>
+      </li>
+    </ul>
+  );
+}
