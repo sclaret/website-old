94a5018c12706a283eb4edb38056cf3b93d1668b
Simon Claret <sclaret@gmail.com>
2023-10-24 21:47:38 -0400

    App: add bootstrap Card boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <>Hello world</>;
+export default function App() {
+  return (
+    <div className="card bg-warning">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
 }
-
-export default App;
